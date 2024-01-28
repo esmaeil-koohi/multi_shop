0f4adc786dc68353b5ecc2d5b9bf6e1dd5ed6687
esmaeil-koohi <esmaeil76koohi@gmail.com>
2024-01-28 17:08:40 +0330

    filters

--- product/urls.py
+++ product/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 app_name = 'product'
 urlpatterns = [
+    path('all', views.ProductListView.as_view(), name='products_list'),
     path('<int:pk>', views.ProductDetailView.as_view(), name='product_detail'),
-    path('/navbar', views.NavbarPartialView.as_view(), name='navbar'),
+    path('navbar', views.NavbarPartialView.as_view(), name='navbar'),
     path('category', views.CategoryStyle.as_view(), name='category'),
 
 ]
--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
+
 from django.shortcuts import render
-from django.views.generic import DetailView, TemplateView
+from django.views.generic import DetailView, TemplateView, ListView
 from product.models import Product, Category
 
 
@@ -24,3 +25,29 @@ class CategoryStyle(TemplateView):
         context = super(CategoryStyle, self).get_context_data()
         context['categories'] = Category.objects.all()
         return context
+
+
+class ProductListView(ListView):
+    template_name = 'product/products_list.html'
+    queryset = Product.objects.all()
+
+    def get_context_data(self, **kwargs):
+        request = self.request
+        colors = request.GET.getlist('color')
+        sizes = request.GET.getlist('size')
+        min_price = request.GET.get('min_price')
+        max_price = request.GET.get('max_price')
+        queryset = Product.objects.all()
+        if colors:
+            queryset = queryset.filter(color__title__in=colors).distinct()
+        if sizes:
+            queryset = queryset.filter(size__title__in=sizes).distinct()
+        if min_price and max_price:
+            queryset = queryset.filter(price__lte=max_price, price__gte=min_price)
+        context = super(ProductListView, self).get_context_data()
+        context['object_list'] = queryset
+        return context
+
+
+
+
